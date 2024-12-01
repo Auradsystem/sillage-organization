486b0ee7224cc236c0a6b9bf1c207eb677d8e8b6
Auradsystem <sammari.dali@gmail.com>
2024-12-01 13:48:32 +0100

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1 style={{ fontSize: '24px', color: 'blue' }}>Sillage</h1>
+      <p style={{ marginTop: '20px' }}>Organisation & Structure</p>
+    </div>
+  );
+}
